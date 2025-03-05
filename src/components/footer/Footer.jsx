b035fcfbad248fcf88572e47a40d6a7f5a4cbf4f
GreenGoblin01002 <erlhal01002@stud.noroff.no>
2025-03-05 13:12:35 +0100

    footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>This is a footer🔥</p>
+            <p>© 2021</p>
+        </footer>
+    );
+}
+
+export default Footer;
